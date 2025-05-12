37c36202bc20ee95301fd1b35ba7a2bc66bf030c
Kunal <kunalpandit5200@gmail.com>
2025-05-12 12:00:14 +0530

    add uat url

--- src/api/serverConstant.js
+++ src/api/serverConstant.js
@@ -1,6 +1,6 @@
 // const baseUrl = "https://api.vroar.ai";
-const baseUrl = "https://dev.api.vroar.ai";
-// const baseUrl = "https://uatapi.mytreks.ai";
+// const baseUrl = "https://dev.api.vroar.ai";
+const baseUrl = "https://uatapi.mytreks.ai";
 // const baseUrl = "http://52.53.170.60";
 const stripeURL = "https://api.stripe.com/v1";
 export const serverApiUrl = {
