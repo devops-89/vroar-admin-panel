06a58aa0ae3a36c00a60b567c0b6d2b4bed94c58
Kunal <kunalpandit5200@gmail.com>
2025-05-26 13:54:09 +0530

    add gallup and strength api

--- src/components/user/notes/gspResults.jsx
+++ src/components/user/notes/gspResults.jsx
@@ -1,6 +1,6 @@
 import { ASSESSMENT_TYPE, COLORS } from "@/utils/enum";
-import { Box, IconButton, Stack, Typography } from "@mui/material";
-import React, { useEffect } from "react";
+import { Box, IconButton, Skeleton, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import file from "@/icons/file.png";
 import Image from "next/image";
 import { roboto } from "@/utils/fonts";
@@ -10,7 +10,8 @@ import userController from "@/api/user";
 const GSPResults = () => {
   const user = useSelector((state) => state.USER);
   const { id } = user;
-
+  let url = "";
+  const [loading, setLoading] = useState(true);
   const getAssessmentResults = () => {
     let body = {
       userId: id,
@@ -20,11 +21,19 @@ const GSPResults = () => {
     userController
       .getUserAssessmentResult(body)
       .then((res) => {
-        console.log("user response", res);
+        // console.log("user response", res);
+        const result = res.data.data.gallupResult;
+        // console.log("first", result);
+        url = result;
+        setLoading(false);
       })
       .catch((err) => {
         console.log("error in gallup results", err);
       });
+  };
+
+  const viewPdf = (url) => {
+    window.open(url, "_blank");
   };
 
   useEffect(() => {
@@ -33,19 +42,27 @@ const GSPResults = () => {
 
   return (
     <div>
-      <Box sx={{ backgroundColor: "#ebebeb", borderRadius: 2, p: 1, mt: 2 }}>
-        <Stack
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-        >
-          <Stack direction={"row"} alignItems={"center"} spacing={2}>
-            <Image src={file} />
-            <Box>
-              <Typography sx={{ fontSize: 15, fontFamily: roboto.style }}>
-                GSP Test Result
-              </Typography>
-              <Typography
+      {loading ? (
+        <Skeleton
+          variant="rectangular"
+          width={1150}
+          height={50}
+          sx={{ mt: 2, borderRadius: 2, p: 1 }}
+        />
+      ) : (
+        <Box sx={{ backgroundColor: "#ebebeb", borderRadius: 2, p: 1, mt: 2 }}>
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Stack direction={"row"} alignItems={"center"} spacing={2}>
+              <Image src={file} />
+              <Box>
+                <Typography sx={{ fontSize: 15, fontFamily: roboto.style }}>
+                  GSP Test Result
+                </Typography>
+                {/* <Typography
                 sx={{
                   fontSize: 12,
                   fontFamily: roboto.style,
@@ -53,14 +70,15 @@ const GSPResults = () => {
                 }}
               >
                 500kb
-              </Typography>
-            </Box>
+              </Typography> */}
+              </Box>
+            </Stack>
+            <IconButton onClick={() => viewPdf(url)}>
+              <Visibility htmlColor={COLORS.BLACK} />
+            </IconButton>
           </Stack>
-          <IconButton>
-            <Visibility htmlColor={COLORS.BLACK} />
-          </IconButton>
-        </Stack>
-      </Box>
+        </Box>
+      )}
     </div>
   );
 };
