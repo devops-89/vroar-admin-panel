8690639635579ed39968c2f73fe9bb6b7952cefd
Kunal Sharma <kunalpandit5200@gmail.com>
2025-06-23 19:02:51 +0530

    add draft in roadmap

--- src/components/roadmap/components/SubmitButton.js
+++ src/components/roadmap/components/SubmitButton.js
@@ -1,11 +1,37 @@
 import { Box, Button } from "@mui/material";
-import { COLORS } from "@/utils/enum";
+import { COLORS, ROADMAP_STATUS } from "@/utils/enum";
 import { roboto } from "@/utils/fonts";
 import Loading from "react-loading";
 
-const SubmitButton = ({ loading, onClick, disabled }) => {
+const SubmitButton = ({ loading, onClick, disabled, draftLoading }) => {
   return (
-    <Box sx={{ textAlign: "end" }}>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        gap: 3,
+      }}
+    >
+      <Button
+        sx={{
+          fontSize: 14,
+          backgroundColor: COLORS.TRANSPARENT,
+          color: COLORS.PRIMARY,
+          fontFamily: roboto.style,
+          mt: 2,
+          width: 120,
+          border: `1px solid ${COLORS.PRIMARY}`,
+        }}
+        onClick={() => onClick(ROADMAP_STATUS.DRAFT)}
+        disabled={disabled}
+      >
+        {draftLoading ? (
+          <Loading type="bars" width={20} height={20} color={COLORS.BLACK} />
+        ) : (
+          "Draft"
+        )}
+      </Button>
       <Button
         sx={{
           fontSize: 14,
@@ -15,7 +41,7 @@ const SubmitButton = ({ loading, onClick, disabled }) => {
           mt: 2,
           width: 120,
         }}
-        onClick={onClick}
+        onClick={() => onClick(ROADMAP_STATUS.PUBLISHED)}
         disabled={disabled}
       >
         {loading ? (
