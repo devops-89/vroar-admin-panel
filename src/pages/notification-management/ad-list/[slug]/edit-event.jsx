273fa082ed2c1c30f33a6d6d89684c6327f6ed07
Kunal Sharma <kunalpandit5200@gmail.com>
2025-07-08 12:44:24 +0530

    remove null value

--- src/pages/notification-management/ad-list/[slug]/edit-event.jsx
+++ src/pages/notification-management/ad-list/[slug]/edit-event.jsx
@@ -1,24 +1,33 @@
 import { metaDataController } from "@/api/metaDataController";
 import EditEventForm from "@/components/event/editEvent";
 import Wrapper from "@/components/wrapper";
+import { setToast } from "@/redux/reducers/toast";
+import { ToastStatus } from "@/utils/enum";
 import withAuth from "@/utils/withAuth";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 
 const EditEvent = () => {
   const router = useRouter();
   const [details, setDetails] = useState(null);
   const id = router.query.slug;
-
+  const dispatch = useDispatch();
   const getEventById = (id) => {
     metaDataController
       .getEventById(id)
       .then((res) => {
-        // console.log("rs", res);
         setDetails(res.data.data);
       })
       .catch((err) => {
         console.log("err", err);
+        dispatch(
+          setToast({
+            open: true,
+            message: "Getting error in fetch details",
+            severity: ToastStatus.ERROR,
+          })
+        );
       });
   };
 
